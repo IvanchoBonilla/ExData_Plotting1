b4625caffffce705782c90f605ee4f4ef528cd86
IvanchoBonilla <ibonilla_ud@hotmail.com>
2016-11-20 21:04:02 -0500

    Plot1.R

--- Scripts/Plot1.R
+++ Scripts/Plot1.R
@@ -5,9 +5,10 @@ DT <- tbl_df(fread("household_power_consumption.txt", na.strings = "?"))
 DT <- mutate(DT, Date=as.Date(t(DT[,1]), "%d/%m/%Y"))
 A <- filter(DT, Date>="2007-02-01", Date<="2007-02-02")
 
+png(filename = "Plot1.png", height=480, width=480, units = "px")
+
 with(A, hist(Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power"))
 
-png(filename = "Plot1.png", height=480, width=480, units = "px")
 dev.off()
 
 rm(DT)
