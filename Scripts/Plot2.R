25b2134da9e599c2834409fcfcd030dce200b196
IvanchoBonilla <ibonilla_ud@hotmail.com>
2016-11-20 21:04:31 -0500

    Plot2

--- Scripts/Plot2.R
+++ Scripts/Plot2.R
@@ -8,10 +8,11 @@ A <- filter(DT, Date>="2007-02-01", Date<="2007-02-02")
 B<-tbl_df(strptime(t(paste(as.character(t(A[,1])), t(A[,2]))), format = "%Y-%m-%d %H:%M:%S"))
 A<-bind_cols(A, B)
 
+png(filename = "Plot2.png", height=480, width=480, units = "px")
+
 with(A, plot(value, Global_active_power, type = "n", ylab = "Global Active Power (kilowatts)", xlab = ""))
 with(A, lines(value, Global_active_power))
 
-png(filename = "Plot2.png", height=480, width=480, units = "px")
 dev.off()
 
 rm(DT)
